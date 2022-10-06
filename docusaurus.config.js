a470716890ab1e2bc58814c14badbe25fc90f171
Anibal Aguila <aa@usen.io>
2022-10-06 22:50:49 +0200

    content: addition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'GetCert',
+  tagline: 'Get your next Certification',
   url: 'https://getcert.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -33,19 +33,21 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+         blog: false,
+//        blog: {
+//          showReadingTime: true,
+//          // Please change this to your repo.
+//          // Remove this to remove the "edit this page" links.
+//          editUrl:
+//            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -72,24 +74,24 @@ const config = {
         contextualSearch: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'GetCert',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Get your next Certification!',
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+//          {
+//            type: 'doc',
+//            docId: 'intro',
+//            position: 'left',
+//            label: 'Tutorial',
+//          },
+//          {to: '/blog', label: 'Blog', position: 'left'},
+//          {
+//            href: 'https://github.com/facebook/docusaurus',
+//            label: 'GitHub',
+//            position: 'right',
+//          },
         ],
       },
       footer: {
